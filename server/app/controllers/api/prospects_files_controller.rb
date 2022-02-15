c5a29d583515da28ff0773850cdc6481e17f9399
jwill-hatchways <jake.williamson1999+hatchways@gmail.com>
2022-02-15 15:55:24 -0500

    Complete functionality for GET prospects_files. Add seed for a prospect job. Fix routes

--- server/app/controllers/api/prospects_files_controller.rb
+++ server/app/controllers/api/prospects_files_controller.rb
@@ -3,5 +3,14 @@ class Api::ProspectsFilesController < ApplicationController
   end
 
   def progress
+    job = ImportJob.find(params.require(:id))
+
+    p job
+
+    if job.user_id != @user.id
+      return render status: 403, json: {message: "The import job does not belong to this user."}
+    end
+
+    return render status: 200, json: {total: job.total, done: job.done}
   end
 end
--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -1,4 +1,15 @@
 user = User.create!(email: "test@test.com", password: "sample")
+import_job = user.import_jobs.create!(
+    total: 100,
+    done: 95,
+    filename: "testfile.csv",
+    email_index: 1,
+    first_name_index: 2,
+    last_name_index: 3,
+    force_overwrite: false,
+    has_headers: true,
+    job_status: "Complete"
+)
 
 for i in 1..3 do
     campaign = user.campaigns.create!(name: "Awesome Campaign #{i}")
