fad7318ffee50a124b8fb0495995163388b185ba
jwill-hatchways <jake.williamson1999+hatchways@gmail.com>
2022-02-16 15:49:02 -0500

    Create unique index on prospects for user_id+email. Authorization given by Deric in Slack chat

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_14_194745) do
+ActiveRecord::Schema.define(version: 2022_02_16_203311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2022_02_14_194745) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.index ["user_id", "email"], name: "index_prospects_on_user_id_and_email", unique: true
     t.index ["user_id"], name: "index_prospects_on_user_id"
   end
 
